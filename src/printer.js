6f7db2e9145be8df00f731d959c710708e47d2a0
Alejandro <jalejandro.rrosario@gmail.com>
2019-10-14 12:16:54 -0500

    Adding console support, base methods
    ** Adding console support (IE older versions)
    ** Basic validations

--- src/printer.js
+++ src/printer.js
@@ -5,7 +5,7 @@
  * @requires JQuery
  * @module
  * @author RRJA (Alejandro del Rio R.)
- * @version 0.0.1
+ * @version 0.0.2
  * @since  20.09.19
 */
 (function (root, factory) {
@@ -28,7 +28,7 @@
      */
     const CONSTANT = {
         MODULE_NAME: "PrintWeb",
-        MODULE_VERSION: "0.0.1",
+        MODULE_VERSION: "0.0.2",
         BASE_REGEX: /data:{1}.*base64,{1}.*/,
         DESTROY_TIMEOUT: 5000,
         FILE_TYPES: {
@@ -60,7 +60,7 @@
      * @member
      * @public
      */
-    var printer = {
+    let printer = {
         FileTypes: {
             JPEG: "image/jpeg",
             PDF: "application/pdf" },
@@ -75,9 +75,9 @@
      * @member
      * @private
      */
-    var supports = !!document.querySelector && !!root.addEventListener;
-    var settings;
-    var doc = null;
+    let supports = !!document.querySelector && !!root.addEventListener;
+    let settings;
+    let doc = null;
 
     /**
      * @description Define the default settings
@@ -85,7 +85,7 @@
      * @member
      * @private
      */
-    var defaults = {
+    let defaults = {
         isSingleFile: true,
         fileData: null,
         fileType: CONSTANT.FILE_TYPES.JPEG,
@@ -99,6 +99,101 @@
         debugger: false
     };
 
+    /**
+     * @description Appends the object attributes to html element
+     * @param {object} element The html element. Eg: div, span, ..., iframe
+     * @param {object} attrObj The object with the attributes. Eg: { id: "idEl", class: "classEl" }
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const addAttributesToElement = function addAttributes(element, attrObj) {
+        for (var attr in attrObj) {
+            if (attrObj.hasOwnProperty(attr)) element.setAttribute(attr, attrObj[attr]);
+        }
+    };
+
+    /**
+     * @description Create a html custom element
+     * @param {string} element The type of html element to create. Eg: div, span, ..., iframe
+     * @param {object} attributes The object with the attributes. Eg: { id: "idEl", class: "classEl" }
+     * @param {array} children The list with the children elements. Eg: ["text"] or [div, anotherDiv]
+     * @return {object} The element created
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const createCustomElement = function (element, attributes, children) {
+        var customElement = document.createElement(element);
+        if (children !== undefined) children.forEach(function (el) {
+            if (el.nodeType) {
+                if (el.nodeType === 1 || el.nodeType === 11) customElement.appendChild(el);
+            } else {
+                customElement.innerHTML += el;
+            }
+        });
+        addAttributesToElement(customElement, attributes);
+        return customElement;
+    };
+
+    /**
+	 * @description Validate if the module is initialized and show an alert in console
+     * @return {boolean} The value if is initialized
+     *
+	 * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+	 */
+    const isInitialize = function () {
+        if (!settings) {
+            console.error(CONSTANT.MODULE_NAME, CONSTANT.MODULE_VERSION, "is not initialized");
+            return false;
+        }
+        return true;
+    };
+
+    /**
+	 * @description Validate if the options are valid
+	 * @return {boolean} The value if are valid
+     *
+	 * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+	 */
+    const isValidSettings = function () {
+        if (settings.fileData === undefined || settings.fileData === null) {
+            console.error("Argument fileData is null or undefined", settings.fileData);
+            return false;
+        }
+        return true;
+    };
+
     return printer;
 
 });
+
+// Console-polyfill. MIT license.
+// https://github.com/paulmillr/console-polyfill
+// Make it safe to do console.log() always.
+(function(global) {
+  'use strict';
+  if (!global.console) {
+    global.console = {};
+  }
+  var con = global.console;
+  var prop, method;
+  var dummy = function() {};
+  var properties = ['memory'];
+  var methods = ('assert,clear,count,debug,dir,dirxml,error,exception,group,' +
+     'groupCollapsed,groupEnd,info,log,markTimeline,profile,profiles,profileEnd,' +
+     'show,table,time,timeEnd,timeline,timelineEnd,timeStamp,trace,warn,timeLog,trace').split(',');
+  while (prop = properties.pop()) if (!con[prop]) con[prop] = {};
+  while (method = methods.pop()) if (!con[method]) con[method] = dummy;
+  // Using `this` for web workers & supports Browserify / Webpack.
+})(typeof window === 'undefined' ? this : window);
