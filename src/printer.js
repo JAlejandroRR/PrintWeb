ba374a867041b2e12e7304f1739a66bfc43ee0e7
Alejandro <jalejandro.rrosario@gmail.com>
2019-10-14 17:24:11 -0500

    Update printer.js
    ** Public and private methods added

--- src/printer.js
+++ src/printer.js
@@ -174,6 +174,559 @@
         return true;
     };
 
+    /**
+     * @description Create the html code of embed object
+     * @param {string} url The url of object
+     * @param {string} type The type of object
+     * @returns {string} The html code with url and type
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const createObjectEmbed = function (url, type) {
+        let objectEmbed = "";
+        objectEmbed += '<object width="100%" height="100%" data="' + url + '" type="' + type + '">';
+        objectEmbed += '<embed width="100%" height="100%" src="' + url + '" type="' + type + '"/>';
+        objectEmbed += '</object>';
+        return objectEmbed;
+    };
+
+    /**
+     * @description Create the html code of image
+     * @param {string} url The url of image
+     * @returns {string} The html code with src
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const createImageEmbed = function (url) {
+        let imageEmbed = "";
+        imageEmbed += '<div class="page-break">';
+        imageEmbed += '<img style="display: block; margin: 3px;" src="' + url + '">';
+        imageEmbed += '</div>';
+        return imageEmbed;
+    };
+
+    /**
+     * @description Decorator to create html documents: contain header and footer
+     * @param {function} action The function to add content to body
+     * @returns {string} The html code
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const createBaseDocument = function (action) {
+        let html = '<html moznomarginboxes mozdisallowselectionprint>' +
+            '<head><title>' + settings.title + '</title>' +
+            '<style type="text/css" media="print">' +
+            '@page { size: auto; margin: 3px; }' +
+            '@media all { .page-break { display: none; } }' +
+            '@media print { .page-break { display: block; page-break-after: always; }' +
+                '#Header, #Footer { display: none!important; } }' +
+            '</style></head><body>';
+        if (typeof (action) === CONSTANT.TYPEOF.FUNCTION) html = action(html);
+        html += '</body></html>';
+        return html;
+    };
+
+    /**
+     * @description Create the documet html for object
+     * @param {object, string} objUrl The objects or string that contain the url for object
+     * @returns {string} The html document code with object(s)
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const createDocumentForObject = function (objUrl) {
+        return createBaseDocument(function (html) {
+            if (!settings.isSingleFile && typeof (objUrl) === CONSTANT.TYPEOF.OBJECT) {
+                for (var i = 0; i < objUrl.length; i++) {
+                    html += createObjectEmbed(objUrl[i], settings.fileType);
+                }
+            } else {
+                html += createObjectEmbed(objUrl, settings.fileType);
+            }
+            return html;
+        });
+    };
+
+    /**
+     * @description Create the documet html for image
+     * @param {object, string} imageUrl The object or string that contain the url for image
+     * @returns {string} The html document code with image(s)
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const createDocumentForImage = function (imageUrl) {
+        return createBaseDocument(function (html) {
+            if (!settings.isSingleFile && typeof (imageUrl) === CONSTANT.TYPEOF.OBJECT) {
+                for (var i = 0; i < imageUrl.length; i++) {
+                    html += createImageEmbed(imageUrl[i]);
+                }
+            } else {
+                html += createImageEmbed(imageUrl);
+            }
+            return html;
+        });
+    }
+
+    /**
+     * @description Create the url from byte array
+     * @param {object} file The byte array of file
+     * @returns {string} The url of file
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const getUrl = function (file) {
+        let docBytes = settings.isSerialize ? JSON.parse(file) : file;
+        let bytes = new Uint8Array(docBytes);
+        let file = new Blob([bytes], { type: settings.fileType });
+        return URL.createObjectURL(file);
+    }
+
+    /**
+     * @description Normalize base64 url, validate if not contain the data header to put it
+     * @param {string} baseUrl The url of file
+     * @returns {string} The url of file normalized
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const normalizeBase64 = function (baseUrl) {
+        let regex = new RegExp(CONSTANT.BASE_REGEX);
+        let normalizeUrl = [];
+        if (!settings.isSingleFile && typeof (baseUrl) === CONSTANT.TYPEOF.OBJECT) {
+            for (var i = 0; i < baseUrl.length; i++) {
+                if (!regex.test(baseUrl[i])) {
+                    normalizeUrl.push("data:" + settings.fileType + ";base64," + baseUrl[i].trim());
+                }
+                normalizeUrl.push(baseUrl[i].trim());
+            }
+        } else {
+            if (!regex.test(baseUrl)) {
+                baseUrl = "data:" + settings.fileType + ";base64," + baseUrl[i].trim();
+            }
+            normalizeUrl= baseUrl.trim();
+        }
+        return normalizeUrl;
+    };
+
+    /**
+     * @description Create the url from byte array of filedata
+     * @returns {string, object} The url(s)
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const createUrlFromByteArray = function () {
+        let url = [];
+        if (!settings.isSingleFile) {
+            for (var i = 0; i < settings.fileData; i++) {
+                url.push(getUrl(settings.fileData[i]));
+            } 
+        } else {
+            url = getUrl(settings.fileData);
+        }
+        return url;
+    };
+
+    /**
+     * @description Open a new window
+     * @param {string} url The url of window
+     * @returns {object} The window opened
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const openWindow = function (url) {
+        url = url || CONSTANT.STRING.EMPTY;
+        return window.open(url, settings.title);
+    };
+
+    /**
+     * @description Write on global document
+     * @param {string} html The html code to write
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const writeOnWindow = function (html) {
+        if (typeof (doc) !== CONSTANT.TYPEOF.UNDEFINED && doc !== null) {
+            html = html || CONSTANT.STRING.EMPTY;
+            doc.document.open();
+            doc.document.write(html);
+            doc.document.close();
+        }
+    };
+
+    /**
+     * @description Print the global document
+     * @param {boolean} close If window will be closed
+     * @param {number} timeOut The time out to print the document
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const printWindow = function (close) {
+        let timeOut = (typeof (settings.printTimeOut) === CONSTANT.TYPEOF.NUMBER
+            ? settings.printTimeOut
+            : (!isNaN(settings.printTimeOut) ? parseInt(settings.printTimeOut) : 1000));
+        setTimeout(function () {
+            doc.focus();
+            doc.print();
+            if (close) doc.close();
+        }, timeOut);
+    };
+
+    /**
+     * @description Open a new frame (create)
+     * @param {string} html The html code to append in frame
+     * @returns {object} The frame opened
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const openFrame = function (html) {
+        html = html || CONSTANT.STRING.EMPTY;
+        let n = Math.floor(Math.random() * 1000);
+        let frameID = CONSTANT.MODULE_NAME + "-" + n;
+        let frame = createCustomElement("iframe", {
+            "id": frameID,
+            "class": CONSTANT.MODULE_NAME
+        }, [html]);
+        document.body.appendChild(frame);
+        return frame;
+    };
+
+    /**
+     * @description Write on frame
+     * @param {object} frame The frame
+     * @param {string} html The html code to write
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const writeOnFrame = function (frame, html) {
+        if (typeof (frame) !== CONSTANT.TYPEOF.UNDEFINED && frame !== null) {
+            html = html || CONSTANT.STRING.EMPTY;
+            frame.contentWindow.document.open();
+            frame.contentWindow.document.write(html);
+            frame.contentWindow.document.close();
+        }
+    };    
+
+    /**
+     * @description Print the frame
+     * @param {number} timeOut The time out to print
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const printFrame = function (frame) {
+        let timeOut = (typeof (settings.printTimeOut) === CONSTANT.TYPEOF.NUMBER
+            ? settings.printTimeOut
+            : (!isNaN(settings.printTimeOut) ? parseInt(settings.printTimeOut) : 1000));
+        setTimeout(function () {
+            frame.contentWindow.focus();
+            frame.contentWindow.print();
+        }, timeOut);
+    };
+
+    /**
+     * @description Remove all frames create by module from DOM
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const destroyAllFrame = function () {
+        let frames = document.getElementsByClassName(CONSTANT.MODULE_NAME);
+        for (var i = frames.length - 1; i >= 0; i--) {
+            frames[i].parentNode.removeChild(frames[i]);
+        }
+    };
+
+    /**
+     * @description Show the file(s) from base64
+     * @param {boolean} print If document will be printed
+     * @param {boolean} close If window will be closed
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const showFromBase64 = function (print, close) {
+        doc = openWindow();
+        let u = normalizeBase64(settings.fileData);
+        let h = null;
+
+        switch (settings.fileType.toLowerCase()) {
+            case CONSTANT.FILE_TYPES.JPEG:
+                h = createDocumentForImage(u);
+                break;
+            case CONSTANT.FILE_TYPES.PDF:
+                h = createDocumentForObject(u);
+                break;
+            default:
+                console.error("The file type", settings.fileType, "is not supported from base64 file format.");
+                break;
+        }
+
+        if (h !== null) {
+            writeOnWindow(h);
+            doc.focus();
+            if (print) printWindow(close);
+        }
+    };
+
+    /**
+     * @description Show the file(s) from byte array
+     * @param {boolean} print If document will be printed
+     * @param {boolean} close If window will be closed
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const showFromByteArray = function (print, close) {
+        doc = openWindow();
+        let u = createUrlFromByteArray();
+        let h = null;
+
+        switch (settings.fileType.toLowerCase()) {
+            case CONSTANT.FILE_TYPES.JPEG:
+            case CONSTANT.FILE_TYPES.PDF:
+                h = createDocumentForObject(u);
+                break;
+            default:
+                console.error("The file type", settings.fileType, "is not supported from byte array file format.");
+                break;
+        }
+
+        if (h !== null) {
+            writeOnWindow(h);
+            doc.focus();
+            if (print) printWindow(close);
+        }
+    };
+
+    /**
+     * @description Print from base64
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const printFrameFromBase64 = function () {
+        let u = normalizeBase64(settings.fileData);
+        let h = null;
+        switch (settings.fileType.toLowerCase()) {
+            case CONSTANT.FILE_TYPES.JPEG:
+                h = createDocumentForImage(u);
+                break;
+            case CONSTANT.FILE_TYPES.PDF:
+                h = createDocumentForObject(u);
+                break;
+            default:
+                console.error("The file type", settings.fileType, "is not supported from base64 file format.");
+                break;
+        }
+
+        if (h !== null) {
+            let f = openFrame();
+            writeOnFrame(f, h);
+            printFrame(f);
+            let timeout = (typeof (settings.printTimeOut) === CONSTANT.TYPEOF.NUMBER
+                ? settings.printTimeOut
+                : (!isNaN(settings.printTimeOut) ? parseInt(settings.printTimeOut) : 1000)) + CONSTANT.DESTROY_TIMEOUT
+            setTimeout(destroyAllFrame, timeout);
+        }
+    };
+
+    /**
+     * @description Print from byte array
+     *
+     * @function
+     * @private
+     * @author RRJA
+     * @version 0.1
+     */
+    const printFrameFromByteArray = function () {
+        let u = createUrlFromByteArray();
+        let h = null;
+        switch (settings.fileType.toLowerCase()) {
+            case CONSTANT.FILE_TYPES.JPEG:
+            case CONSTANT.FILE_TYPES.PDF:
+                h = createDocumentForObject(u);
+                break;
+            default:
+                console.error("The file type", settings.fileType, "is not supported from byte array file format.");
+                break;
+        }
+
+        if (h !== null) {
+            let f = openFrame();
+            writeOnFrame(f, h);
+            printFrame(f);
+            let timeout = (typeof (settings.printTimeOut) === CONSTANT.TYPEOF.NUMBER
+                ? settings.printTimeOut
+                : (!isNaN(settings.printTimeOut) ? parseInt(settings.printTimeOut) : 1000)) + CONSTANT.DESTROY_TIMEOUT
+            setTimeout(destroyAllFrame, timeout);
+        }
+    };
+
+    /**
+     * @description Destroy
+     *
+     * @function
+     * @public
+     * @author RRJA
+     * @version 0.1
+     */
+    printer.destroy = function () {
+        // If plugin isn't already initialized, stop
+        if (!settings) return;
+
+        // Reset variables
+        doc = null;
+        settings = null;
+    };
+
+    /**
+     * @description Initialize Plugin
+     * @param {object} options User options
+     *
+     * @function
+     * @public
+     * @author RRJA
+     * @version 0.1
+     */
+    printer.init = function (options) {
+        if (!supports) return;
+        printer.destroy();
+        settings = jQuery.extend(defaults, options || {});
+        if (settings.debugger) console.info(CONSTANT.MODULE_NAME, CONSTANT.MODULE_VERSION, "is initialize:", settings);
+    };
+
+    /**
+     * @description Unlink previous window opened
+     *
+     * @function
+     * @public
+     * @author RRJA
+     * @version 0.1
+     */
+    printer.resetWindow = function () {
+        if (!isInitialize()) return;
+        doc = null;
+    };
+
+    /**
+     * @description Show the file
+     * @param {object} options User options
+     *
+     * @function
+     * @public
+     * @author RRJA
+     * @version 0.1
+     */
+    printer.show = function (options) {
+        if (!isInitialize() || !isValidSettings()) return;
+        settings = jQuery.extend(settings, options || {});
+        if (settings.debugger) console.info("Function show start...");
+        if (settings.debugger) console.info("Function show(options):", options);
+
+        switch (settings.fileFormat.toLowerCase()) {
+            case CONSTANT.FILE_FORMATS.BASE64:
+                showFromBase64(settings.showAndPrint, false);
+                break;
+            case CONSTANT.FILE_FORMATS.BYTE_ARRAY:
+                showFromByteArray(settings.showAndPrint, false);
+                break;
+            default:
+                console.error("Argument fileFormat invalid.", settings.fileFormat, "Not supported.");
+                break;
+        }
+
+        if (settings.debugger) console.info("Function show end...");
+    };
+
+    /**
+     * @description Print the file
+     * @param {object} options User options
+     *
+     * @function
+     * @public
+     * @author RRJA
+     * @version 0.1
+     */
+    printer.print = function (options) {
+        if (!isInitialize() || !isValidSettings()) return;
+        settings = jQuery.extend(settings, options || {});
+        if (settings.debugger) console.info("Function print start...");
+        if (settings.debugger) console.info("Function print(options):", options);
+
+        if (typeof (doc) !== CONSTANT.TYPEOF.UNDEFINED && doc !== null) {
+            printWindow(false);
+        } else {
+            switch (settings.fileFormat.toLowerCase()) {
+                case CONSTANT.FILE_FORMATS.BASE64:
+                    if (settings.printOnNewWindow) {
+                        showFromBase64(true, settings.printCloseNewWindow);
+                    } else {
+                        printFrameFromBase64();
+                    }
+                    break;
+                case CONSTANT.FILE_FORMATS.BYTE_ARRAY:
+                    if (settings.printOnNewWindow) {
+                        showFromByteArray(true, settings.printCloseNewWindow);
+                    } else {
+                        printFrameFromByteArray();
+                    }
+                    break;
+                default:
+                    console.error("Argument fileFormat invalid.", settings.fileFormat, "Not supported.");
+                    break;
+            }
+        }
+
+        if (settings.debugger) console.info("Function print end...");
+    };
+
     return printer;
 
 });
